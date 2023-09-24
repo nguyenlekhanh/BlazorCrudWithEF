0d56d88707d7d39c9bfd6b661126032acbaa5c2b
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-24 13:35:51 -0500

    add SuperHero service implement first controller implement first razor page

--- BlazorCrudWithEF/Client/Program.cs
+++ BlazorCrudWithEF/Client/Program.cs
@@ -1,3 +1,5 @@
+global using BlazorCrudWithEF.Client.Services.SuperHeroService;
+global using BlazorCrudWithEF.Shared;
 using BlazorCrudWithEF.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+
 
 await builder.Build().RunAsync();
---
+++ BlazorCrudWithEF/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+
+namespace BlazorCrudWithEF.Client.Services.SuperHeroService
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient _http;
+
+        public SuperHeroService(HttpClient http) 
+        {   
+            _http = http;
+        }
+
+        public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+        public List<Comic> Comics { get; set; } = new List<Comic>();
+        public HttpClient Http { get; }
+
+        public Task GetComics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SuperHero> GetSingleHero(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task GetSuperHeroes()
+        {
+            var result = await _http.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+            if(result != null)
+            {
+                Heroes = result;
+            }
+            
+        }
+    }
+}
