e47ad94117c64a58141063d1a5c33b40e0b51ce6
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-24 18:39:29 -0500

    add dbcontext run first migration

---
+++ BlazorCrudWithEF/Server/Controllers/Data/DataContext.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System.Reflection;
+
+namespace BlazorCrudWithEF.Server.Controllers.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comic>().HasData(
+                new Comic { Id = 1, Name = "Marvel" },
+                new Comic { Id = 2, Name = "DC" }
+            );
+
+            modelBuilder.Entity<SuperHero>().HasData(
+                new SuperHero
+                {
+                    Id = 1,
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    HeroName = "Spiderman",
+                    ComicId = 1
+                },
+                new SuperHero
+                {
+                    Id = 1,
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    HeroName = "Batman",
+                    ComicId = 2
+                }
+            );
+        }
+
+        public DbSet<SuperHero> SuperHero{ get; set; }
+
+        public DbSet<Comic> Comic { get; set; }
+    }
+}
--- BlazorCrudWithEF/Server/Program.cs
+++ BlazorCrudWithEF/Server/Program.cs
@@ -1,5 +1,6 @@
 global using BlazorCrudWithEF.Shared;
-
+global using Microsoft.EntityFrameworkCore;
+global using BlazorCrudWithEF.Server.Controllers.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 
 var app = builder.Build();
 
